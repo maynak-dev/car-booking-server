2424beaddf83ae5b0b0429d97487798210ef164e
maynak-dev <work.maynak@gmail.com>
2026-03-14 01:32:36 +0530

    booking payment change2

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const { authenticate, authorizeAdmin } = require('../middlewares/auth.middleware');
-const { getAllBookings, updateBookingStatus, getAllUsers, blockUser, getStats } = require('../controllers/admin.controller');
+const { 
+  getAllBookings, 
+  updateBookingStatus, 
+  updatePaymentStatus,  
+  blockUser, 
+  getStats 
+} = require('../controllers/admin.controller');
 const router = express.Router();
 
+// All routes below require admin authentication
 router.use(authenticate, authorizeAdmin);
 
 router.get('/bookings', getAllBookings);
 router.put('/bookings/:id/status', updateBookingStatus);
-router.get('/users', getAllUsers);
+router.put('/bookings/:id/payment', updatePaymentStatus);
 router.put('/users/:id/block', blockUser);
 router.get('/stats', getStats);
-router.put('/bookings/:id/payment', updatePaymentStatus);
 
 module.exports = router;
