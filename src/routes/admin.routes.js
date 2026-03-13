f5db3ffb798457171b9589a749305a41aecdf179
maynak-dev <work.maynak@gmail.com>
2026-03-14 02:26:56 +0530

    routes modified

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -4,6 +4,7 @@ const {
   getAllBookings, 
   updateBookingStatus, 
   updatePaymentStatus,  
+  getAllUsers, 
   blockUser, 
   getStats 
 } = require('../controllers/admin.controller');
@@ -15,6 +16,7 @@ router.use(authenticate, authorizeAdmin);
 router.get('/bookings', getAllBookings);
 router.put('/bookings/:id/status', updateBookingStatus);
 router.put('/bookings/:id/payment', updatePaymentStatus);
+router.get('/users', getAllUsers);
 router.put('/users/:id/block', blockUser);
 router.get('/stats', getStats);
 
